e3cb94b75b0de88824dfb732fe46dbad23ef38fd
Wesley Hall <wesleyahall@gmail.com>
2017-02-22 14:25:30 -0600

    Fixing boilerplate

--- app/index.js
+++ app/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
 
-import App from './containers/App'
+import App from './components/App'
 import reducers from './reducers'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore)
